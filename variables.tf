a2f72123dcf544d8e6db8836c0e949a2a66380b6
Martyna Maciejewska <44441407+maartynaa@users.noreply.github.com>
2026-04-04 15:05:23 +0200

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,28 @@
+variable "project_id" {
+  description = "ID projektu w GCP"
+  type        = string
+}
+
+variable "region" {
+  description = "Region dla zasobów"
+  type        = string
+  default     = "europe-central2" # Warszawa
+}
+
+variable "zone" {
+  description = "Strefa dla VM"
+  type        = string
+  default     = "europe-central2-a"
+}
+
+variable "instance_type" {
+  description = "Typ maszyny wirtualnej"
+  type        = string
+  default     = "e2-standard-2" # e2-small (2GB RAM) okazał się niewystarczający
+}
+
+variable "instance_type" {
+  description = "Typ maszyny wirtualnej"
+  type        = string
+  default     = "e2-standard-2" # e2-small (2GB RAM) okazał się niewystarczający
+}
